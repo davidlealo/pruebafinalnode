eabe144f04c638a9d78df1634bda43fa2b7ca199
davidlealo <davidlealo@gmail.com>
2023-04-22 15:17:32 -0400

    Segundo requerimiento completado

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -3,7 +3,7 @@ const server = require("../index");
 
 describe("Operaciones CRUD de cafes", () => {
 
-    //Primera parte del desafío
+    //Primer requerimiento
     it('la ruta GET /cafes devuelve un status code 200... continua-->', async ()=>{
         const respuesta = await request(server).get('/cafes').send()
         const status = respuesta.statusCode
@@ -16,4 +16,12 @@ describe("Operaciones CRUD de cafes", () => {
         const arreglo = body
         expect(arreglo).toBeInstanceOf(Array)
     })
+
+        //Segundo requerimiento
+        it("Comprueba que se obtiene un código 404 al intentar eliminar un café con un id que no existe", async () => {
+            const id = 11;
+            const response = await request(server).delete(`/cafes/:${id}`).set("Authorization", "token").send();
+            expect(response.statusCode).toBe(404);
+        })
+
 });
