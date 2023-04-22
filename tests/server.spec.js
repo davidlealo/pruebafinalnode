e709c252012fdffaebf18bd635fd8a7fcae1ceab
davidlealo <davidlealo@gmail.com>
2023-04-22 15:14:40 -0400

    Primer test ahora el tipo de dato

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -4,10 +4,16 @@ const server = require("../index");
 describe("Operaciones CRUD de cafes", () => {
 
     //Primera parte del desafío
-    it('la ruta GET /cafes devuelve un status code 200 y el tipo de dato recibido es un arreglo con por lo menos 1 objeto', async ()=>{
+    it('la ruta GET /cafes devuelve un status code 200... continua-->', async ()=>{
         const respuesta = await request(server).get('/cafes').send()
         const status = respuesta.statusCode
         const esperado = 200
         expect(status).toBe(esperado)
+    })
+
+    it('viene de --> ...el tipo de dato recibido es un arreglo con por lo menos 1 objeto', async ()=>{
+        const { body } = await request(server).get("/cafes").send()
+        const arreglo = body
+        expect(arreglo).toBeInstanceOf(Array)
     })
 });
