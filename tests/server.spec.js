7ccb47d2cf0db2fe91010ba51df5e732d904ca72
davidlealo <davidlealo@gmail.com>
2023-04-22 14:39:49 -0400

    Primer test corregido

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -5,7 +5,7 @@ describe("Operaciones CRUD de cafes", () => {
 
     //Primera parte del desafío
     it('la ruta GET /cafes devuelve un status code 200 y el tipo de dato recibido es un arreglo con por lo menos 1 objeto', async ()=>{
-        const respuesta = (await request(server).get('/cafes')).send()
+        const respuesta = await request(server).get('/cafes').send()
         const status = respuesta.statusCode
         const esperado = 200
         expect(status).toBe(esperado)
